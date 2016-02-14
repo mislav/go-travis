c1d769bb1fda260bfebe7bda3b497296b3616835
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-02-14 13:53:20 +1300

    Rudimentary extraction of repo slug from git remotes

---
+++ config/context.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var originNames = []string{"upstream", "github", "origin"}
+
+func RepoSlugFromGit() string {
+	out, err := exec.Command("git", "remote", "-v").CombinedOutput()
+	if err != nil {
+		return ""
+	}
+
+	remoteRe := regexp.MustCompile(`(.+)\s+(.+)\s+\((push|fetch)\)`)
+	githubRe := regexp.MustCompile(`[@/](ssh.)?github.com(:/?|/)([^/]+)/([^/]+)`)
+	found := map[string]string{}
+
+	for _, line := range strings.Split(string(out), "\n") {
+		matches := remoteRe.FindAllStringSubmatch(line, -1)
+		if matches != nil {
+			remoteName := matches[0][1]
+			remoteUrl := matches[0][2]
+			matches = githubRe.FindAllStringSubmatch(remoteUrl, -1)
+			if matches != nil {
+				repoOwner := matches[0][3]
+				repoName := strings.TrimSuffix(matches[0][4], ".git")
+				found[remoteName] = repoOwner + "/" + repoName
+			}
+		}
+	}
+
+	for _, remoteName := range originNames {
+		if slug, set := found[remoteName]; set {
+			return slug
+		}
+	}
+
+	for _, slug := range found {
+		return slug
+	}
+
+	return ""
+}
