646e99fe09854553fb1b7fb1f2457b79940cc05e
Lennard Wolf <lennardwolf@live.de>
2016-04-14 13:10:09 +0200

    update whoami command to request from travis

--- commands/whoami.go
+++ commands/whoami.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"io"
+
+	"github.com/HPI-BP2015H/go-travis/client"
 	"github.com/fatih/color"
 	"github.com/mislav/go-utils/cli"
 )
@@ -10,6 +13,19 @@ func init() {
 }
 
 func whoamiCmd(cmd *cli.Cmd) {
+	params := map[string]string{}
+	res, err := client.Travis().PerformAction("user", "current", params)
+	defer res.Body.Close()
+	if err != nil {
+		color.Red("Error: Could not connect to Travis! \n" + err.Error())
+		color.Yellow("Fall back to asking github.")
+		whoamiGithub()
+		return
+	}
+	io.Copy(cmd.Stdout, res.Body)
+}
+
+func whoamiGithub() {
 	github := LoginToGitHub()
 	user, _, err := github.Users.Get("")
 	if err != nil {
