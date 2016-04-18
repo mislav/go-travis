749aefb958a89718c6c4bfb99d629082d9e2ad83
Lennard Wolf <lennardwolf@live.de>
2016-04-18 16:52:24 +0200

    improves branches command a lot

--- commands/branches.go
+++ commands/branches.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"os"
+	"sort"
+	"strconv"
 
 	"github.com/HPI-BP2015H/go-travis/client"
 	"github.com/HPI-BP2015H/go-utils/cli"
@@ -17,16 +19,35 @@ type Branches struct {
 }
 
 type Branch struct {
-	Name       string      `json:"name"`
-	LastBuild  *Build      `json:"last_build"`
-	Repository *Repository `json:"repo"`
+	Name          string      `json:"name"`
+	LastBuild     *Build      `json:"last_build"`
+	Repository    *Repository `json:"repo"`
+	DefaultBranch bool        `json:"default_branch"`
+}
+
+type byBuildNumber []Branch
+
+func (b byBuildNumber) Len() int {
+	return len(b)
+}
+func (b byBuildNumber) Swap(i, j int) {
+	b[i], b[j] = b[j], b[i]
+}
+func (b byBuildNumber) Less(i, j int) bool {
+	n, _ := strconv.Atoi(b[i].LastBuild.Number)
+	m, _ := strconv.Atoi(b[j].LastBuild.Number)
+	if b[j].DefaultBranch {
+		return m > n
+	} else {
+		return n > m
+	}
 }
 
 func branchesCmd(cmd *cli.Cmd) {
 	params := map[string]string{
 		"repository.slug": os.Getenv("TRAVIS_REPO"),
+		"include":         "repository.default_branch",
 	}
-
 	res, err := client.Travis().PerformAction("branches", "find", params)
 	if err != nil {
 		panic(err)
@@ -35,20 +56,28 @@ func branchesCmd(cmd *cli.Cmd) {
 		cmd.Stderr.Printf("Unexpected HTTP status: %d\n", res.StatusCode)
 		cmd.Exit(1)
 	}
-
 	branches := Branches{}
 	res.Unmarshal(&branches)
-
+	sort.Sort(byBuildNumber(branches.Branches))
+	var maxLengthName int
+	maxLengthNumber := len(branches.Branches[1].LastBuild.Number)
 	for _, branch := range branches.Branches {
-		printBranch(branch)
+		if maxLengthName < len(branch.Name) {
+			maxLengthName = len(branch.Name)
+		}
+	}
+	format := "%-" + strconv.Itoa(maxLengthName+3) + "s"
+	for _, branch := range branches.Branches {
+		printBranch(branch, format, maxLengthNumber)
 	}
 }
 
-func printBranch(branch Branch) {
-	color.Yellow("%s:  ", branch.Name)
-	c := color.New(color.FgRed, color.Bold).PrintfFunc()
+func printBranch(branch Branch, format string, maxLengthNumber int) {
+	y := color.New(color.FgYellow, color.Bold).PrintfFunc()
+	y(format, branch.Name)
+	c := color.New(color.FgRed).PrintfFunc()
 	if branch.LastBuild.State == "passed" {
-		c = color.New(color.FgGreen, color.Bold).PrintfFunc()
+		c = color.New(color.FgGreen).PrintfFunc()
 	}
-	c("  #%s  %s\n", branch.LastBuild.Number, branch.LastBuild.State)
+	c("#%-"+strconv.Itoa(maxLengthNumber)+"s %s\n", branch.LastBuild.Number, branch.LastBuild.State)
 }
